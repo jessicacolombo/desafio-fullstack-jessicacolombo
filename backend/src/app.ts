9524668f091585734e9c345d8d57979c08612c06
jessicacolombo <jessicacolombo384@gmail.com>
2023-03-22 16:34:27 -0300

    add cors configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,9 +5,24 @@ import { errorHandler } from "./errors/errorHandler";
 import { sessionRoutes } from "./routes/session.routes";
 import { userRoutes } from "./routes/user.routes";
 import { contactsRoutes } from "./routes/contacts.routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    allowedHeaders: [
+      "sessionId",
+      "Content-Type",
+      "Authorization",
+      "authorization",
+    ],
+    origin: ["http://localhost:3000"],
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  })
+);
+
 app.use("/users", userRoutes);
 app.use("/login", sessionRoutes);
 app.use("/contacts", contactsRoutes);
