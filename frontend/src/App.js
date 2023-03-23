e65f88a9e6d700700e88a0a6e200309a6ce14eea
jessicacolombo <jessicacolombo384@gmail.com>
2023-03-23 18:53:01 -0300

    add context and routes components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import { RoutesMain } from "./routes";
+import { GlobalStyle } from "./styles/global";
+import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./contexts/userContext";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <UserProvider>
+        <RoutesMain />
+      </UserProvider>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
